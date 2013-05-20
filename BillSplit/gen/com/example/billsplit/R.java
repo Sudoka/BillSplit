4a31241e582b656d0b931ece42d07ec84b01a4f0
Daniel Cashman <dan.a.cashman@gmail.com>
2013-05-20 16:11:56 -0700

    Committing first draft of Event and Participant classes.  Many errors remain due to reliance on not-yet-created BalanceChange and Account classes.

--- BillSplit/gen/com/example/billsplit/R.java
+++ BillSplit/gen/com/example/billsplit/R.java
@@ -43,49 +43,52 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_blank=0x7f030000;
-        public static final int activity_event_main=0x7f030001;
-        public static final int activity_events=0x7f030002;
-        public static final int activity_events_list_fragment=0x7f030003;
-        public static final int activity_login=0x7f030004;
-        public static final int activity_main=0x7f030005;
-        public static final int activity_tutorial=0x7f030006;
-        public static final int simple_list_item=0x7f030007;
+        public static final int activity_bogus=0x7f030001;
+        public static final int activity_event_main=0x7f030002;
+        public static final int activity_events=0x7f030003;
+        public static final int activity_events_list_fragment=0x7f030004;
+        public static final int activity_login=0x7f030005;
+        public static final int activity_main=0x7f030006;
+        public static final int activity_tutorial=0x7f030007;
+        public static final int simple_list_item=0x7f030008;
     }
     public static final class menu {
         public static final int blank=0x7f070000;
-        public static final int event_main=0x7f070001;
-        public static final int events=0x7f070002;
-        public static final int events_list=0x7f070003;
-        public static final int login=0x7f070004;
-        public static final int main=0x7f070005;
-        public static final int tutorial=0x7f070006;
+        public static final int bogus=0x7f070001;
+        public static final int event_main=0x7f070002;
+        public static final int events=0x7f070003;
+        public static final int events_list=0x7f070004;
+        public static final int login=0x7f070005;
+        public static final int main=0x7f070006;
+        public static final int tutorial=0x7f070007;
     }
     public static final class string {
-        public static final int action_forgot_password=0x7f05000e;
+        public static final int action_forgot_password=0x7f05000f;
         public static final int action_settings=0x7f050001;
-        public static final int action_sign_in_register=0x7f05000c;
-        public static final int action_sign_in_short=0x7f05000d;
+        public static final int action_sign_in_register=0x7f05000d;
+        public static final int action_sign_in_short=0x7f05000e;
         public static final int app_name=0x7f050000;
         public static final int btn_create_event_text=0x7f050005;
-        public static final int btn_get_started_text=0x7f050016;
-        public static final int btn_view_tutorial_text=0x7f050015;
-        public static final int error_field_required=0x7f050013;
-        public static final int error_incorrect_password=0x7f050012;
-        public static final int error_invalid_email=0x7f050010;
-        public static final int error_invalid_password=0x7f050011;
+        public static final int btn_get_started_text=0x7f050017;
+        public static final int btn_view_tutorial_text=0x7f050016;
+        public static final int error_field_required=0x7f050014;
+        public static final int error_incorrect_password=0x7f050013;
+        public static final int error_invalid_email=0x7f050011;
+        public static final int error_invalid_password=0x7f050012;
         public static final int hello_world=0x7f050002;
-        public static final int login_progress_signing_in=0x7f05000f;
+        public static final int login_progress_signing_in=0x7f050010;
         /**  Strings related to login 
          */
-        public static final int prompt_email=0x7f05000a;
-        public static final int prompt_password=0x7f05000b;
+        public static final int prompt_email=0x7f05000b;
+        public static final int prompt_password=0x7f05000c;
         public static final int title_activity_blank=0x7f050008;
+        public static final int title_activity_bogus=0x7f050009;
         public static final int title_activity_event_main=0x7f050007;
         public static final int title_activity_events=0x7f050003;
         public static final int title_activity_events_list_fragment=0x7f050006;
-        public static final int title_activity_login=0x7f050009;
+        public static final int title_activity_login=0x7f05000a;
         public static final int title_activity_tutorial=0x7f050004;
-        public static final int welcome_message=0x7f050014;
+        public static final int welcome_message=0x7f050015;
     }
     public static final class style {
         /** 
---
+++ BillSplit/src/com/example/billsplit/Event.java
@@ -0,0 +1,212 @@
+package com.example.billsplit;
+
+import java.util.*;
+
+public class Event {
+	private String creatorGID;
+	private String name;
+	private ArrayList<Particpant> participants;
+	private ArrayList<BalanceChange> txns;
+	private String category;
+	private Date dateCreated;
+	private Date lastModified;
+	
+	/* 
+	 * requires: creatorGID != null
+	 *           name != null
+	 */
+	public Event(String creatorGID, String name){
+		assert(creatorGID != null && name != null)
+		this.creatorGID = creatorGID;
+		this.name = name;
+		participants = new ArrayList<Partiicpant>();
+		txns = new ArrayList<BalanceChange> txns;
+		category = "None";
+		dateCreated = Date();
+		lastModified = dateCreated;
+	}
+	
+	/* 
+	 * requires: creatorGID != null
+	 *           name != null
+	 *           category != null
+	 */
+	public Event(String creatorGID, String name, String category){
+		assert(creatorGID != null && name != null && category != null);
+		this.creatorGID = creatorGID;
+		this.name = name;
+		participants = new ArrayList<Partiicpant>();
+		txns = new ArrayList<BalanceChange>();
+		this.category = category;
+		dateCreated = Date();
+		lastModified = dateCreated;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public String getCreatorGID(){
+		return creatorGID;
+	}
+	
+	public Collection<Partiicpant> getParticpants(){
+		return (Collection) participants;
+	}
+	
+	/*
+	 * requires name != null
+	 */
+	public boolean isParticipant(String name){
+		assert(name != null);
+		for(Participant p : participants){
+			if(p.getName().equals(name)){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/*
+	 * requires name != null
+	 */
+	public boolean isParticipantByGID(String gid){
+		assert(gid != null);
+		for(Participant p : participants){
+			/*
+			 * dacashman - getGID() missing from Account contract 
+			 */
+			if(p.getAccount().getGID().equals(gid)){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public Collection<BalanceChange> getBalanceChanges(){
+		return (Collection) txns;
+	}
+	
+	public String getCategory(){
+		return category;
+	}
+	
+	public void addBalanceChange(BalanceChange newBalanceChange){
+		assert(newBalanceChange != null);
+		txns.add(newBalanceCHange);
+		return;
+	}
+	
+	/*
+	 * dacashman - contract requires date to be non-null, but 
+	 * 	this is wrong.  null should mean "infinity."  A null 
+	 * start_date would mean "get all dates from beginning of event."
+	 *  requires: beginDate be before endDate.
+	 */
+	public Collection<BalanceChanges> getBalanceChangesByDate(Date beginDate, Date endDate){
+		assert(beginDate.before(endDate));
+		ArrayList<BalanceChanges> matching = new ArrayList<BalanceChanges>();
+		if(beginDate == null){
+			if(endDate == null){
+				return (Collection) txns;
+			}else{
+				for(BalanceChange b : txns){
+					if(endDate.after(b.getDate())){
+						matching.add(b);
+					}
+				}
+			}
+		}else if(endDate == null){
+			for(BalanceChange b : txns){
+				if(beginDate.before(b.getDate())){
+					matching.add(b);
+				}
+			}
+		}else{
+			for(BalanceChange b : txns){
+				Date bDate = b.getDate();
+				if(beginDate.before(bDate) && endDate.after(bDate)){
+					matching.add(b);
+				}
+			}
+		}
+		return (Collection) matching;
+	}
+	
+	/*
+	 * require isParticipant(name).
+	 */
+	public Participant getParticipantByName(String name){
+		assert(isParticipant(name));
+		for(Participant p : participants){
+			if(p.getName().equals(name)){
+				return p;
+			}
+		}
+		/* should never get here: complain if so.*/
+		assert(false);
+		return null;
+	}
+	
+	/*
+	 * require isParticipantByGID(gid).
+	 */
+	public Participant getParticipantByGID(String gid){
+		assert(gid != null && isParticipantByGID(gid));
+		for(Participant p : participants){
+			if(p.getAccount().getGID().equals(gid)){
+				return p;
+			}
+		}
+		/* should never get here: complain if so.*/
+		assert(false);
+		return null;
+	}
+	
+	/*
+	 * requires: participant not already in event (by name and 
+	 * thus GID).
+	 */
+	public void addParticipant(Participant newParticipant){
+		assert(!isParticipant(newParticipant.getName()));
+		participants.add(newParticipant);
+		return;
+	}
+	
+	public void removeParticipant(Participant ParticipantToRemove){
+		assert(isParticipant(ParticipantToRemove));
+		participants.remove(ParticipantToRemove);
+		return;
+	}
+	
+		
+	/* dacashman - addBalanceChange, removeBalanceChange need to be 
+	 * 	added to the contract.
+	 */
+		
+	
+	public void setCategory(String category){
+		assert(category != null);
+		this.category = category;
+		return;
+	}
+	
+	/*
+	 * setCreatorGID should not exist or be in contract 
+	 */
+	
+	public void setName(String name){
+		assert(name != null);
+		this.name = name;
+		return;
+	}
+	
+	/* update - recaclulates balances based on all transactions 
+	 *  (shouldn't really be used) dacashman
+	 */
+	public void update(){
+		System.out.print("Not yet implemented\n");
+		return;
+	}
+	
+}
