f99ea0f2739b94b9b185ffb00724bc0ee76c5963
Tatenda <tatenda@cs.ucsd.edu>
2013-05-20 14:36:38 -0700

    Initial version of DataCapture interface. Pending evaluation.

---
+++ BillSplit/src/com/example/billsplit/DataCapture.java
@@ -0,0 +1,40 @@
+package com.example.billsplit;
+import java.util.ArrayList;
+
+
+public class DataCapture {
+  public class Item{
+    private float cost;
+    private String name;
+    
+    public float getCost(){
+      return cost;
+    }
+    
+    public String getName(){
+      return name;
+    }
+    
+    private ArrayList<Item> itemList;
+    public void DataCapture(){
+      itemList = new ArrayList<Item>();
+    }
+    
+    public ArrayList<Item> getItemList(){
+      return itemList;
+    }
+    public void setItemList(ArrayList<Item> newItemList){
+      return;
+    }
+    public boolean addItem(Item item, boolean update){
+      return false;
+    }
+    
+    public boolean removeItem(int itemId){
+      return false;
+    }
+    public boolean editItem(int itemId){
+      return false;
+    }
+  }
+}
