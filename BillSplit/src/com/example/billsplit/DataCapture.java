2a776c96541a1e5591c1ee74f785dd26e8b3c7cb
Tatenda <tatenda@cs.ucsd.edu>
2013-05-20 14:48:01 -0700

    The correct initial API...

--- BillSplit/src/com/example/billsplit/DataCapture.java
+++ BillSplit/src/com/example/billsplit/DataCapture.java
@@ -1,6 +1,35 @@
 package com.example.billsplit;
 import java.util.ArrayList;
 
+/*OCR TEST 
+ */
+import org.apache.http.HttpHost;
+
+import org.apache.http.HttpResponse;
+
+import org.apache.http.client.methods.HttpGet;
+
+import org.apache.http.client.methods.HttpPost;
+
+import org.apache.http.client.params.ClientPNames;
+
+import org.apache.http.client.params.CookiePolicy;
+
+import org.apache.http.conn.params.ConnRoutePNames;
+
+import org.apache.http.entity.StringEntity;
+
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import org.apache.http.params.BasicHttpParams;
+
+import org.apache.http.params.HttpConnectionParams;
+
+import org.apache.http.params.HttpParams;
+
+import org.apache.http.util.EntityUtils;
+/*
+ */
 
 public class DataCapture {
   public class Item{
@@ -14,27 +43,27 @@ public class DataCapture {
     public String getName(){
       return name;
     }
-    
-    private ArrayList<Item> itemList;
-    public void DataCapture(){
-      itemList = new ArrayList<Item>();
-    }
-    
-    public ArrayList<Item> getItemList(){
-      return itemList;
-    }
-    public void setItemList(ArrayList<Item> newItemList){
-      return;
-    }
-    public boolean addItem(Item item, boolean update){
-      return false;
-    }
-    
-    public boolean removeItem(int itemId){
-      return false;
-    }
-    public boolean editItem(int itemId){
-      return false;
-    }
+  }
+  
+  private ArrayList<Item> itemList;
+  public void DataCapture(){
+    itemList = new ArrayList<Item>();
+  }
+  
+  public ArrayList<Item> getItemList(){
+    return itemList;
+  }
+  public void setItemList(ArrayList<Item> newItemList){
+    return;
+  }
+  public boolean addItem(Item item, boolean update){
+    return false;
+  }
+  
+  public boolean removeItem(int itemId){
+    return false;
+  }
+  public boolean editItem(int itemId){
+    return false;
   }
 }
