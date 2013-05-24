7044e71a0e337b2a874c2751a2d3c7bb1a6da047
Daniel <dlgallag@eng.ucsd.edu>
2013-05-24 04:28:51 -0700

    Trying out some junit testing. First test tried is for the Item() class b/c it is simple. To run: in eclipse select the ItemTest.java file, then have eclipse run it as a junit test.

---
+++ BillSplit/junit/billsplit/engine/ItemTest.java
@@ -0,0 +1,104 @@
+/**
+ * 
+ */
+package billsplit.engine;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Daniel
+ *
+ */
+public class ItemTest {
+	
+	Item item;
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		this.item = new Item("testItem",50.0);
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		this.item = null;
+	}
+
+	/**
+	 * Test method for {@link billsplit.engine.Item#getCost()}.
+	 */
+	@Test
+	public final void testGetCost() {
+		assertEquals(50.0,item.getCost(),10);
+	}
+
+	/**
+	 * Test method for {@link billsplit.engine.Item#setCost(double)}.
+	 */
+	@Test
+	public final void testSetCost() {
+		item.setCost(100.0);
+		assertEquals(100.0,item.getCost(),10);
+	}
+
+	/**
+	 * Test method for {@link billsplit.engine.Item#getName()}.
+	 */
+	@Test
+	public final void testGetName() {
+		assertEquals("testItem",item.getName());
+	}
+
+	/**
+	 * Test method for {@link billsplit.engine.Item#setName(java.lang.String)}.
+	 */
+	@Test
+	public final void testSetName() {
+		item.setName("newname");
+		assertEquals("newname",item.getName());
+	}
+
+	/**
+	 * Test method for {@link billsplit.engine.Item#getCategory()}.
+	 */
+	@Test
+	public final void testGetCategory() {
+		assertEquals("",item.getCategory());
+	}
+
+	/**
+	 * Test method for {@link billsplit.engine.Item#setCategory(java.lang.String)}.
+	 */
+	@Test
+	public final void testSetCategory() {
+		item.setCategory("testCategory");
+		assertEquals("testCategory",item.getCategory());
+	}
+
+	/**
+	 * Test method for {@link billsplit.engine.Item#getDetails()}.
+	 */
+	@Test
+	public final void testGetDetails() {
+		assertEquals("",item.getDetails());
+	}
+
+	/**
+	 * Test method for {@link billsplit.engine.Item#setDetails(java.lang.String)}.
+	 */
+	@Test
+	public final void testSetDetails() {
+		item.setDetails("testDetails");
+		assertEquals("testDetails",item.getDetails());
+	}
+
+}
