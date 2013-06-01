01134e36d8ce31d70a6f06f823830f73183e9e7f
manuelespinoza <m8espino@eng.ucsd.edu>
2013-05-31 23:49:29 -0700

    item lists layout changed

--- BillSplit/src/billsplit/ui/ItemAdapter.java
+++ BillSplit/src/billsplit/ui/ItemAdapter.java
@@ -1,8 +1,12 @@
 package billsplit.ui;
 import java.util.List;
 
+import com.billsplit.R;
+
 import billsplit.engine.Item;
+import android.app.Activity;
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -11,21 +15,37 @@ import android.widget.TextView;
 public class ItemAdapter extends ArrayAdapter<Item> {
 
 	List<Item> itemList = null;
+	private Context context;
+	private int layoutResourceId;
 	
 	public ItemAdapter(Context context, int layoutResourceId,
 			List<Item> objects) {
 		super(context, layoutResourceId, objects);
 		itemList = objects;
+		this.context = context;
+		this.layoutResourceId = layoutResourceId;
 	}
 	
 	@Override
     public View getView(int position, View convertView, ViewGroup parent) {
-		TextView txt = new TextView(getContext());
+		/*TextView txt = new TextView(getContext());
 		txt.setText(itemList.get(position).getName() + "$"+itemList.get(position).getCost());
 		
 		txt.setTag(itemList.get(position));
 		return txt;
+		*/
+		View v = convertView;
+		LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+        v = inflater.inflate(layoutResourceId, parent, false);
+        
+        
+		TextView name = (TextView) v.findViewById(R.id.layout_txtItemName);
+		name.setText(itemList.get(position).getName());
 		
+		TextView price = (TextView) v.findViewById(R.id.layout_txtPrice);
+		price.setText("$ "+String.valueOf(itemList.get(position).getCost()));
+		v.setTag(itemList.get(position));
+		return v;
 	}
 
 	
