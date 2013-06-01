91ed4178cbfdd488591292898be942fec83eb5d0
Daniel <dangacal@gmail.com>
2013-05-31 20:47:37 -0700

    new file: EngineTests.java. Junit test suite; runs all junit test classes for engine.

---
+++ BillSplit/junit/billsplit/engine/EngineTests.java
@@ -0,0 +1,11 @@
+package billsplit.engine;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ItemTest.class, TransactionTest.class })
+public class EngineTests {
+
+}
